addb56180222184a1da70735b4803675d3749827
darwin-sd <dan.sirbu.sd@gmail.com>
2026-07-15 19:21:22 +0300

    Change registry type

--- src/lib/app/app-registry.ts
+++ src/lib/app/app-registry.ts
@@ -1,11 +1,11 @@
 import { AppDefinition } from "./types";
 import AboutMeApp from "@/components/app/about-me";
 
-export const APP_REGISTRY: Record<string, AppDefinition> = {
-  "about-me": {
+export const APP_REGISTRY: AppDefinition[] = [
+  {
     id: "about-me",
     title: "About Me",
     iconUrl: "/about-me.png",
     component: AboutMeApp,
   },
-};
+];
